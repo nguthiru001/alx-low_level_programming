26278c6ec895c2c21c4b917db56362893847cfbe
nguthiru001 <iannguthiru@icloud.com>
2021-06-26 11:53:00 -0700

    first commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf(""Programming is like building a multilingual puzzle");
+int main(4-puts.c) {
+  printf("Programming is like building a multilingual puzzle");
    return 0;
 }
