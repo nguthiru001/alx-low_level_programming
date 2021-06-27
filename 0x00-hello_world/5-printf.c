1e24754ef60c6c5e3c9bee1158c0ee8a18358240
nguthiru001 <iannguthiru@icloud.com>
2021-06-27 05:49:32 -0700

    first commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
-
+/**
+* main - some desc
+*
+* Return: 0 for success
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
+printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
